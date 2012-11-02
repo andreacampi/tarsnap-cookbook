5a7d08e6d338546210fb8bba867ae1cda27028fa
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-02 23:41:40 +0100

    Bump version for Debian support and other pull requests.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures tarsnap"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.10"
+version          "0.1.0"
 
 recipe "tarsnap", "Installs tarsnap"
 
