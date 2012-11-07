125213c77ebff8ca1260534eea60ce9b84253559
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-07 08:27:28 +0100

    Bump for release again; no actual changes.
    I messed up the push to the community site.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures tarsnap"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.2"
 
 recipe "tarsnap", "Installs tarsnap"
 
